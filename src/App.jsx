831cfdfe26093a649795a06bae6fe1a036b43d70
Raluca COANDA <cdraluca2@hotmail.fr>
2026-08-04 16:36:46 +0200

    rajout data local

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,21 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
-import Galerie from './pages/Galerie'
+import Gallery from './pages/Gallery'
 import { SelectionProvider } from './composants/SelectionContext'
 import { DicoProvider } from './composants/lang/Dico'
 
 function App() {
   return (
     <DicoProvider>
-    <SelectionProvider>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/gallery" element={<Galerie />} />
-      </Routes>
-    </SelectionProvider>
+      <SelectionProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/galerie" element={<Gallery />} />
+        </Routes>
+      </SelectionProvider>
     </DicoProvider>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ export default function Home() {
     if (viewMode && dataSource) {
       setStage('transition')
       setTimeout(() => {
-        navigate('/gallery')
+        navigate('/galerie')
       }, 1500)
     } else {
       setStage('idle')
