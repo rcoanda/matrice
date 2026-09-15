44f7fc39bd162d773f08825663df59b688b5398f
Raluca COANDA <cdraluca2@hotmail.fr>
2026-09-15 14:43:00 +0200

    correction texture vide

--- src/composants/canvas/view/common/SpriteView.jsx
+++ src/composants/canvas/view/common/SpriteView.jsx
@@ -7,7 +7,7 @@ export default function SpriteView({ image, video, position, onClick }) {
 
   return (
     <sprite position={position} onClick={onClick}>
-      <spriteMaterial map={texture} transparent size={size} />
+      {texture && <spriteMaterial map={texture} transparent size={size} />}
     </sprite>
   )
 }
