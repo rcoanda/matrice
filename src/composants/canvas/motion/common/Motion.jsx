7a0ddb9e14533588e2dff996f28d4a0631cd9a70
Raluca COANDA <cdraluca2@hotmail.fr>
2026-09-18 17:44:30 +0200

    commentaire

--- src/composants/canvas/motion/common/Motion.jsx
+++ src/composants/canvas/motion/common/Motion.jsx
@@ -1,6 +1,7 @@
 // Photo qui se déplace sur UNE trajectoire, la trajectoire étant pilotée par
 // le hook de mouvement reçu en props (ex: useCircularMotion, useLemniscateMotion).
 // moving photos
+//cree le mouvement/le film (toutes les frames / le comportement) de la source (une liste des artwork) sur la trajectoire (points) deja calculées dans utils/trajectoire
 import { usePhotoMeshTexture } from '../../../../hooks/model/usePhotoMeshTexture'
 
 export default function Motion({ useMotion, motionArgs, length, width, index, source }) {
